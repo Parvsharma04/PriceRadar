f0d4b98c60598201dcd48c4901da52df2cf3c9f9
Parv Sharma <sharmaparv.2004@gmail.com>
2024-10-31 02:49:33 +0530

    Adds basic routing

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { NextUIProvider } from '@nextui-org/react';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Layout from "./components/Layout";
+import "./index.css";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import NotFound from "./pages/not-found";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <NextUIProvider>
+      <Router>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Layout>
+      </Router>
+    </NextUIProvider>
+  </StrictMode>
+);
