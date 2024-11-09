18731709d96c6844e4a068c28cc241f669357cf0
Nikhil Guleria <gulerianikhil69@gmail.com>
2024-11-09 13:36:28 +0530

    updated searchbar

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,7 +4,7 @@ import SearchBar from "../components/search.jsx"; // Make sure this path is corr
 const Home = () => {
   return (
     <>
-      <div className="h-100% bg-[#6833ff] text-[#020008] flex justify-center p-4">
+      <div className="h-100% bg-[#6833ff] flex justify-center p-4">
         <div className="text-center">
           <h1 className="mb-6 md:font-bold text-6xl">Welcome to Mobile Price Comparator</h1>
           
