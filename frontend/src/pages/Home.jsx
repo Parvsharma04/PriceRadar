94447736dfa19076c73f13abacb2ff55e7a53681
Nikhil Guleria <gulerianikhil69@gmail.com>
2024-11-09 13:30:43 +0530

    SearchBar

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import SearchBar from "../components/search.jsx"; // Make sure this path is correct
 
 const Home = () => {
   return (
     <>
-      <div className="h-screen bg-black">Home Page</div>
+      <div className="h-100% bg-[#6833ff] text-[#020008] flex justify-center p-4">
+        <div className="text-center">
+          <h1 className="mb-6 md:font-bold text-6xl">Welcome to Mobile Price Comparator</h1>
+          
+          {/* Render the SearchBar component */}
+          <SearchBar />
+          
+          <p className="mt-4 text-lg">Search for your favorite mobile and compare prices</p>
+        </div>
+      </div>
     </>
   );
 };
